5103644c817920d148e25a1b375b96a66f0bc8c6
muhammadalvin92 <alvinbomber87@gmail.com>
2023-08-22 12:11:36 +0700

    Pages Main, Templates, Option

---
+++ resources/views/Pages/Option.blade.php
@@ -0,0 +1,16 @@
+@extends('Pages.Templates')
+@section('judul_tab','Option Kemendikbud')
+
+@section('content')
+<link rel="stylesheet" href=" {{ asset('../css/Pages/Option.css')}}">
+
+    <div class="optionContent text-center">
+        <h1 class="fw-bold my-1">UMKM</h1>
+        <p class="fw-semibold my-1">Selamat Datang Di UMKM</p>
+        <P class="fw-semibold my-1">Masuk Dengan Akun UMKM Anda Untuk Melanjutkan</P>
+        <div class="btn">
+            <a class="btn btn-blue" href="#" role="button"><p>Masuk</p></a>
+            <a class="btn btn-blue" href="#" role="button"><p>Registrasi</p></a>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/Pages/Templates.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('judul_tab')</title>
+    <link rel="stylesheet" href=" {{ asset('../css/Pages/Templates.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <img class="bgTop" src="{{asset('../../images/bgColorfull1.png')}}" alt="background">
+    <img class="bgBottom" src="{{asset('../../images/bgColorfull2.png')}}" alt="background">
+
+
+    <div class="mainContent">
+        @yield('content')
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
