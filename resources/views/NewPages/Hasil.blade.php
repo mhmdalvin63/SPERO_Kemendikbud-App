54aea2ac00455fb78962c5fea56c13c0d959c226
muhammadalvin92 <alvinbomber87@gmail.com>
2023-08-29 10:35:23 +0700

    Update

--- resources/views/NewPages/Hasil.blade.php
+++ resources/views/NewPages/Hasil.blade.php
@@ -232,9 +232,9 @@
  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
     <script src="text/javascript"></script>
     <script>
-        var slideIndex = 1;
-        showDivs(slideIndex);
-        
+      var slideIndex = 0;
+      showDivs(slideIndex);
+      
         function plusDivs(n) {
           showDivs(slideIndex += n);
         }
@@ -246,9 +246,9 @@
         function showDivs(n) {
           var i;
           var x = document.getElementsByClassName("mySlides");
-          var dots = document.getElementsByClassName("demo");
-        //   if (n > 4) {slideIndex = 1}    
-        //   if (n < 1) {slideIndex = 4}
+          var dots = document.getElementsByClassName("dots-button");
+          // if (n > 6) {slideIndex = 1}    
+          // if (n < 1) {slideIndex = 6}
           for (i = 0; i < x.length; i++) {
             x[i].style.display = "none";  
           }
@@ -256,6 +256,7 @@
             dots[i].className = dots[i].className.replace(" w3-red", "");
           }
           x[slideIndex-1].style.display = "block";  
+          // x[slideIndex-1].style.transform = "translateY(-50%)";  
           dots[slideIndex-1].className += " w3-red";
         }
         </script>
